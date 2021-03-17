ea566ca8d560a4a7c7e5ff60e06031200c19983b
Benjamin Hadizamani <benjamin.hadizamani@gmail.com>
2021-03-17 02:54:21 +0100

    create template for RESTapi with routes

---
+++ api-server/routes/stocks.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+// todo implement all routes under 'stocks/...' like in user the way your front end team has specified them in the contract definition
+// todo fill in mocks
+
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router
