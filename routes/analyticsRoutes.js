ac0224dbc632b79977949008c2368f30d1ff4b1f
kristiyansakalyan <57534207+kristiyansakalyan@users.noreply.github.com>
2021-04-06 21:25:15 +0200

    Removed some comments.

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -7,34 +7,6 @@ const portfolioFetcher = require('../data-analytics/dynamic_data/portfolio-fetch
 
 const router = express.Router();
 
-/**
- * @swagger
- * /analytics/backtest:
- *  get:
- *   description: Backtests a real or a virtual portfolio for a given period of time
- *   summary: Backtests a real or a virtual portfolio for a given period of time
- *   produces:
- *      - application/json  
- *   tags:
- *    - analytics
- *   parameters:
- *      - in: body
- *        name: portfolioId
- *        required: true
- *        description: ID of portfolio
- *        type: number
- *        name: startDate
- *        required: true
- *        description: The starting date for the backtest
- *        type: string        
- *        name: endDate
- *        required: true
- *        description: The ending date for the backtest
- *        type: string         
- *   responses:
- *      '200':
- *          description: Success.
- */
 router.get('/backtest/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
@@ -59,26 +31,6 @@ router.get('/backtest/:id', async (req, res) => {
     // If no errors return results
 })
 
-/**
- * @swagger
- * /analytics/diversification:
- *  get:
- *   description: Calculates the weighted distribution of stocks in a portfolio among different criterion
- *   summary: Calculates the weighted distribution of stocks in a portfolio among different criterion
- *   produces:
- *      - application/json
- *   tags:
- *    - analytics
- *   parameters:
- *     - name: id
- *       in: path
- *       description: ID of portfolio
- *       required: true
- *       type: number
- *   responses:
- *      '200':
- *          description: Success.
- */
  router.get('/diversification/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
@@ -103,26 +55,6 @@ router.get('/backtest/:id', async (req, res) => {
     // If no errors return results
 })
 
-/**
- * @swagger
- * /analytics/dividends:
- *  get:
- *   description: Calculates the weighted average dividend and also returns the dividends per stock
- *   summary: Calculates the weighted average dividend and also returns the dividends per stock
- *   produces:
- *      - application/json
- *   tags:
- *    - analytics
- *   parameters:
- *     - name: id
- *       in: path
- *       description: ID of portfolio
- *       required: true
- *       type: number
- *   responses:
- *      '200':
- *          description: Success.
- */
  router.get('/dividends/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
@@ -148,26 +80,6 @@ router.get('/backtest/:id', async (req, res) => {
 
 })
 
-/**
- * @swagger
- * /analytics/peratios:
- *  get:
- *   description: Calculates the weighted average of PERatio of a portfolio and returns also the PERatios per stock
- *   summary: Calculates the weighted average of PERatio of a portfolio and returns also the PERatios per stock
- *   produces:
- *      - application/json
- *   tags:
- *    - analytics
- *   parameters:
- *     - name: id
- *       in: path
- *       description: ID of portfolio
- *       required: true
- *       type: number
- *   responses:
- *      '200':
- *          description: Success.
- */
  router.get('/peratios/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
@@ -192,26 +104,6 @@ router.get('/backtest/:id', async (req, res) => {
     // If no errors return results
 })
 
-/**
- * @swagger
- * /analytics/gainLoss:
- *  get:
- *   description: Calculates the weighted average of Gain or Loss of a portfolio and returns also the gain or loss per stock
- *   summary: Calculates the weighted average of Gain or Loss of a portfolio and returns also the gain or loss per stock
- *   produces:
- *      - application/json
- *   tags:
- *    - analytics
- *   parameters:
- *     - name: id
- *       in: path
- *       description: ID of portfolio
- *       required: true
- *       type: number
- *   responses:
- *      '200':
- *          description: Success.
- */
  router.get('/gainLoss/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
@@ -237,26 +129,6 @@ router.get('/backtest/:id', async (req, res) => {
 
 })
 
-/**
- * @swagger
- * /analytics/volatilityCorrelation:
- *  get:
- *   description: Calculates the volatility and correlation of stocks within a portfolio
- *   summary: Calculates the volatility and correlation of stocks within a portfolio
- *   produces:
- *      - application/json
- *   tags:
- *    - analytics
- *   parameters:
- *     - name: id
- *       in: path
- *       description: ID of portfolio
- *       required: true
- *       type: number
- *   responses:
- *      '200':
- *          description: Success.
- */
  router.get('/volatilityCorrelation/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
@@ -282,26 +154,6 @@ router.get('/backtest/:id', async (req, res) => {
 
 })
 
-/**
- * @swagger
- * /analytics/sharpeRatio:
- *  get:
- *   description: Calculates the sharpe ratio of stocks within a portfolio
- *   summary: Calculates the sharpe ratio of stocks within a portfolio
- *   produces:
- *      - application/json
- *   tags:
- *    - analytics
- *   parameters:
- *     - name: id
- *       in: path
- *       description: ID of portfolio
- *       required: true
- *       type: number
- *   responses:
- *      '200':
- *          description: Success.
- */
  router.get('/sharpeRatio/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
@@ -326,26 +178,6 @@ router.get('/backtest/:id', async (req, res) => {
     // If no errors return results
 })
 
-/**
- * @swagger
- * /analytics/debtEquity:
- *  get:
- *   description: Calculates the weighted average of debt/equity of a portfolio and returns also the debt/equity per stock
- *   summary: Calculates the weighted average of debt/equity of a portfolio and returns also the debt/equity per stock
- *   produces:
- *      - application/json
- *   tags:
- *    - analytics
- *   parameters:
- *     - name: id
- *       in: path
- *       description: ID of portfolio
- *       required: true
- *       type: number
- *   responses:
- *      '200':
- *          description: Success.
- */
  router.get('/debtEquity/:id', async (req, res) => {
     let response = {error: "", success: {}};
     const id = req.params.id;
