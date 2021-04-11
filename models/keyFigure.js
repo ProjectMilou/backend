64ac83c1b6cb5ec11bf4a1c4fa3b667b6fb1f935
Yucel Ozdemir <contact@yucelozdemir.com>
2021-04-11 21:03:08 +0200

    stock: added dividend and keyFigure models, and added all relevant data to db

---
+++ models/keyFigure.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const KeyFigureSchema = new mongoose.Schema({
+    symbol: {
+        type: String,
+        required: true,
+        index: true,
+        trim: true,
+        unique: true,
+    },
+    keyFigures: [{
+        fiscalDateEnding: {
+            type: String,
+        },
+        reportedDate: {
+            type: String,
+        },
+        reportedEPS: {
+            type: String,
+        },
+        estimatedEPS: {
+            type: String,
+        },
+        surprise: {
+            type: String,
+        },
+        surprisePercentage: {
+            type: String,
+        },
+    }],
+}, {
+    versionKey: false
+});
+
+const KeyFigure = mongoose.model("KeyFigure", KeyFigureSchema);
+
+module.exports = KeyFigure;
