db49344ab7bbcdf13560b8d54b1ffb08feedfd4f
Gabit Sagindykov <gabitii00@gmail.com>
2021-04-14 11:18:38 +0200

    added comments and deleted unused in email.js

--- auth/email.js
+++ auth/email.js
@@ -9,15 +9,18 @@ const SES_CONFIG = {
     region:'eu-central-1'
 };
 
+//conecting to aws
 const AWS_SES = new AWS.SES(SES_CONFIG);
 
 let  sendConfirmationEmail =  (recipientEmail,id, confirmationCode) =>{
+    //creating json object with data needed in template
     var templateData = {};
     templateData.id = id;
     templateData.confirmationCode= confirmationCode;
     let params = {
+        //setting up parameters for email
         Source: 'info@milou.io',
-        Template:'EmailConfirmation',
+        Template:'EmailConfirmation', //template name saved on aws
         Destination:{
             ToAddresses: [
                 recipientEmail
@@ -25,34 +28,20 @@ let  sendConfirmationEmail =  (recipientEmail,id, confirmationCode) =>{
         },
         ReplyToAddresses: [],
         TemplateData: JSON.stringify(templateData),
-        /*
-        Message: {
-            Body: {
-                Html:{
-                    Charset: 'UTF-8',
-                    Data: '<h1>Email Confirmation</h1>' +
-                        '<h2> Hello !</h2>' +
-                        '<p> Thank you for registration. Please confirm your email by clicking on the following link</p>' +
-                        '<a href="http://milou.io/confirm/${id}/${confirmationCode}"> Click here</a>',
-                },
-            },
-            Subject:{
-                Charset: 'UTF-8',
-                Data: 'confirmation',
-            }
-        },*/
     };
     return AWS_SES.sendTemplatedEmail(params).promise();
 }
 
 let sendPasswordResetEmail = (recipientEmail, id,name, confirmationCode) =>{
+    //creating json object with data needed in template
     var templateData = {};
     templateData.name = name;
     templateData.id = id;
     templateData.confirmationCode= confirmationCode;
     let params = {
+        //setting up parameters for email
         Source: 'info@milou.io',
-        Template: 'PasswordReset',
+        Template: 'PasswordReset', //template name saved on aws
         Destination :{
             ToAddresses: [
                 recipientEmail
