4a2b44299d1a039d54a61e702aa1dd7775190713
Rebecca Ghidini <rebeccaghi2001@gmail.com>
2021-04-17 18:17:50 +0200

    removed deprecation warning

--- db/index.js
+++ db/index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');//https://mongoosejs.com/docs/index.html
 mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 module.exports = function (app) {
 
