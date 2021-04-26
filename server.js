4d3cef072726842b3c839b39b9c5425c345e89ac
Oğuzhan Göller <goller.oguzhan@gmail.com>
2021-04-26 14:46:18 +0300

    implement testing for stocks part with jest (#69)
    * add tests for stocks part
    
    * add async to secrets

--- server.js
+++ server.js
@@ -1,37 +1,27 @@
 'use strict';
 
 // init is required to access AWS Secret Manager before running any other code.
-const init = async () => {
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const getAnalyticsRoute = require('./routes/analyticsRoutes')
+    
+const getPortfolioRoute = require('./routes/portfolio');
+const getStocksRoute = require('./routes/stocks');
+const stockWorkers = require('./workers/stocks_worker')
+const chartWorkers = require('./workers/charts_worker')
+    
+const cron = require("node-cron");
+    
+const app = express();
+
+async function initialize() {
     await require('./secret/secret')();
-
-    const express = require('express');
-    const bodyParser = require('body-parser');
-    const cors = require('cors');
-
-    const getAnalyticsRoute = require('./routes/analyticsRoutes')
-    const getUserRoute = require('./routes/user');
-    const getPortfolioRoute = require('./routes/portfolio');
-    const getStocksRoute = require('./routes/stocks');
-    const stockWorkers = require('./workers/stocks_worker')
-    const chartWorkers = require('./workers/charts_worker')
+    require('./auth/auth');
+    const { PORT = 3000 } = process.env;
     const swaggerJsDoc = require('swagger-jsdoc');
     const swaggerUi = require('swagger-ui-express');
-    const cron = require("node-cron");
-    require('./auth/auth');
-
-    // Constants
-    const { PORT = 3000 } = process.env;
-    const HOST = '0.0.0.0';
-    const app = express();
-
-    cron.schedule("0 4 * * *", () => {
-        stockWorkers.updateAllStocks();
-        chartWorkers.updateAllCharts();
-    }, {
-        timezone: "Europe/Berlin"
-    });
-
-    // swagger setup
     const swaggerOptions = {
         encoding: "utf-8",
         swaggerDefinition: {
@@ -53,23 +43,35 @@ const init = async () => {
     const swaggerDocs = swaggerJsDoc(swaggerOptions);
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-    // database setup
-    require("./db/index.js")(app);
-
-    app.get('/', (req, res) => {
-        res.statusCode = 200;
-        res.send("please visit <b> https://api.milou.io/api-docs/ </b> for documentation")
-    })
-
-    app.use(bodyParser.json());
-    app.use(cors());
-
+    const getUserRoute = require('./routes/user');
     app.use('/user', getUserRoute);
-    app.use('/portfolio', getPortfolioRoute);
-    app.use('/stocks', getStocksRoute);
-    app.use('/analytics', getAnalyticsRoute);
-
-    app.listen(process.env.PORT || 3000);
-    console.log(`Running on http://${HOST}:${PORT}`);
+    require("./db/index.js")(app);
 }
-init().then();
+
+if(process.env.NODE_ENV != 'test') {
+    initialize();
+}
+
+cron.schedule("0 4 * * *", () => {
+    stockWorkers.updateAllStocks();
+    chartWorkers.updateAllCharts();
+}, {
+    timezone: "Europe/Berlin"
+});
+
+// database setup
+
+
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.send("please visit <b> https://api.milou.io/api-docs/ </b> for documentation")
+})
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/portfolio', getPortfolioRoute);
+app.use('/stocks', getStocksRoute);
+app.use('/analytics', getAnalyticsRoute);
+
+module.exports = app;
